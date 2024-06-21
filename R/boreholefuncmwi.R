8bcdd637746c7275cb078dc13339475914705193
J-preisig <preisigjuliette@gmail.com>
2024-06-21 15:18:44 +0200

    Title and description

---
+++ R/boreholefuncmwi.R
@@ -0,0 +1,25 @@
+#' boreholefuncmwi: Analysis of boreholes' functionality in Northern Malawi
+#'
+#' The data is obtained from a survey conducted in Wasambo in Karonga district (Northen Malawi). This data is about boreholes and handpumps that people mostly rely on for drinking water, secondary to wells and rivers. The data were collected in June 2013.
+#'
+#' @format A tibble with 6 rows and 17 variables
+#' \describe{
+#'   \item{id}{Identification number of the observation}
+#'   \item{date}{Date the survey was filled out}
+#'   \item{role}{Main role of the respondent}
+#'   \item{committee_members}{How many people are on the Hand pump borehole Committee?}
+#'   \item{last_meeting_participants}{About how many community members attended the last meeting? (99 is don?t know)}
+#'   \item{annual_budget}{How much money do you need to manage this borehole without financial problems annually?}
+#'   \item{village}{Name of the village}
+#'   \item{number_households}{How many households use this hand pump borehole?}
+#'   \item{tariff_costs_consider}{What costs were considered when setting the tariff or user fee?}
+#'   \item{tariff_frequency}{How often is the tariff/user fee collected?}
+#'   \item{tariff_amount}{How much is the tariff/user fee (in Kwacha)?}
+#'   \item{total_money}{How much do you source to support operations and maintenance annually?}
+#'   \item{tariff_hh_number}{How many households in the community paid a water fee the last time it was collected?}
+#'   \item{distance_materials}{How far away are/were the materials you use for hand pump borehole repairs?}
+#'   \item{service_provider}{Is there a service provider or someone responsible for operating and/or maintaining this hand pump borehole or water system?}
+#'   \item{preventive_maintenance}{Do you conduct preventive maintenance?}
+#'   \item{functional_status}{Functional status of the borehole}
+#' }
+"boreholefuncmwi"
